ac3fa821949e9b27da8c8cefb6618420449d6c89
gmkseta <gmkseta@gmail.com>
2018-11-11 22:27:37 +0900

    mylist 안해썼네

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'trades/list'
-  get 'trades/:id/my_list'=>'trades#my_list'
+  get 'trades/:id/my_list'=>'trades#my_list',as: 'trades_my_list'
   get 'trades/:id/detail'=>'trades#detail' ,as: 'trade_detail'
   post 'trades/mktr'=>'trades#mktr'
   get 'home/home'
