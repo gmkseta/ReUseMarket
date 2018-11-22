eceab7ea957911ea12062b937939f9b0731c2915
gmkseta <gmkseta@gmail.com>
2018-11-22 11:06:06 +0900

    되나보자

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -4,6 +4,7 @@ class Chat < ApplicationRecord
   after_create :notify_pusher, on: :create
 
   def notify_pusher
+    Pusher.cluster="ap3"
     Pusher.trigger("trade-#{self.trade.id}-channel", 'send_message', {
       message: self.message,
       user: self.user,
