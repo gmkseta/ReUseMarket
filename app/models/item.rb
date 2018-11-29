dc827e016f154e42194b372d852430acadfc2c4f
gmkseta <gmkseta@gmail.com>
2018-11-29 15:32:14 +0900

    허재 딸러표시 실화냐

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :trades , dependent: :destroy
   mount_uploaders :item_images, ItemImageUploader
+  validates :item_images, presence:{message:"이미지를 등록 해야합니다"}
 end
