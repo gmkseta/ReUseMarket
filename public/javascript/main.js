2be8d1fb5ee5cf00a00e03c73b80eab855dc38e2
gmkseta <gmkseta@gmail.com>
2018-12-03 16:45:19 +0900

    빨리끝내자 ..

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,48 +1,66 @@
+;
+(function($) {
 
-;(function($) {
-
-   'use strict'
-         var flexProduct = function() {
-             $('.flexslider').flexslider({
-                 animation: "slide",
-                 controlNav: "thumbnails"
-             });
-         }; // Flex Product
-        var goTop = function(){
-            var gotop = $('.go-top');
-            $(window).on('scroll', function() {
-                if ($(this).scrollTop() > 500) {
-                    gotop.addClass('show');
-                } else {
-                    gotop.removeClass('show');
-                }
-            });
-            gotop.on('click', function() {
-                $('html, body').animate({ scrollTop: 0}, 800, 'easeInOutExpo');
-                return false;
-            });
-        }; // Go Top
-
-        var scroll = function() {
-            var scrollDown = $('.scroll-down a');
-            scrollDown.on('click', function() {
-                $('html, body').animate({ scrollTop: $( $(this).attr('href') ).offset().top - 30}, 800);
-                return false;
-            });
-        } // Scroll
-
-        var removePreloader = function() {
-            $('.preloader').fadeOut('2s',function () {
-                $(this).remove();
-            });
-        }; // Remove Preloader
-
-    // Dom Ready
-    $(function() {
-        flexProduct();
-        goTop();
-        scroll();
-        removePreloader();
+  'use strict'
+  var flexProduct = function() {
+    $('.flexslider').flexslider({
+      animation: "slide",
+      controlNav: "thumbnails"
     });
+  }; // Flex Product
+  var goTop = function() {
+    var gotop = $('.go-top');
+    $(window).on('scroll', function() {
+      if ($(this).scrollTop() > 500) {
+        gotop.addClass('show');
+      } else {
+        gotop.removeClass('show');
+      }
+    });
+    gotop.on('click', function() {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 800, 'easeInOutExpo');
+      return false;
+    });
+  }; // Go Top
+
+  var scroll = function() {
+    var scrollDown = $('.scroll-down a');
+    scrollDown.on('click', function() {
+      $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top - 30
+      }, 800);
+      return false;
+    });
+  } // Scroll
+
+  var removePreloader = function() {
+    $('.preloader').fadeOut('2s', function() {
+      $(this).remove();
+    });
+  }; // Remove Preloader
+  var Filterbox = function(){
+      var buttonFilter = $('.filter');
+      var buttonHideFilter = $('.box-filter .hider');
+      $('.box-filter').hide();
+      buttonFilter.on('click',function(){
+          $('.box-filter').slideDown();
+      });
+
+      buttonHideFilter.on('click',function(){
+          $('.box-filter').slideUp();
+      });
+  };  // Filterbox
+
+
+  // Dom Ready
+  $(function() {
+    flexProduct();
+    goTop();
+    scroll();
+    removePreloader();
+    Filterbox();
+  });
 
 })(jQuery);
