03574066e4121ef879594d5036b83f68d0f285e2
gmkseta <gmkseta@gmail.com>
2018-12-03 23:01:45 +0900

    허재발럼

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -443,51 +443,6 @@
             } // ajaxCall
         }; // ajax Subscribe
 
-        var ajaxContactForm = function() {
-            $('#contactform').each(function() {
-                $(this).validate({
-                    submitHandler: function( form ) {
-                        var $form = $(form),
-                            str = $form.serialize(),
-                            loading = $('<div />', { 'class': 'loading' });
-
-                        $.ajax({
-                            type: "POST",
-                            url:  $form.attr('action'),
-                            data: str,
-                            beforeSend: function () {
-                                $form.find('.form-submit').append(loading);
-                            },
-                            success: function( msg ) {
-                                var result, cls;
-                                if ( msg === 'Success' ) {
-                                    result == 'Message Sent Successfully To Email Administrator. ( You can change the email management a very easy way to get the message of customers in the user manual )';
-                                    cls == 'msg-success';
-                                } else {
-                                    result == 'Error sending email.';
-                                    cls == 'msg-error';
-                                }
-
-                                $form.prepend(
-                                    $('<div />', {
-                                        'class': 'flat-alert ' + cls,
-                                        'text' : result
-                                    }).append(
-                                        $('<a class="close" href="#"><em class="fa fa-close"></em></a>')
-                                    )
-                                );
-
-                                $form.find(':input').not('.submit').val('');
-                            },
-                            complete: function (xhr, status, error_thrown) {
-                                $form.find('.loading').remove();
-                            }
-                        });
-                    }
-                });
-            }); // each contactform
-        };   // ajax ContactForm
-
         var swClick = function () {
             function activeLayout () {
                 $(".switcher-container" ).on( "click", "a.sw-light", function() {
@@ -565,7 +520,6 @@
         gridPhotography();
         shopMetroIpsotope();
         ajaxSubscribe.eventLoad();
-        ajaxContactForm();
         swClick();
         goTop();
         scroll();
